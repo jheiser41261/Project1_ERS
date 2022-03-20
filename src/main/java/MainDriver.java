d6cc910eb92f66d75d99757ca1e59ffc22c91faf
Justin Heiser <justin.a.heiser@gmail.com>
2022-03-20 16:03:41 -0400

    P1 frontend design

--- src/main/java/MainDriver.java
+++ src/main/java/MainDriver.java
@@ -1,23 +1,30 @@
 import controllers.UserController;
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class MethodTest {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(9000);
+        Javalin app = Javalin.create(config -> {
+            config.addStaticFiles("/", Location.CLASSPATH);
+        }).start(9000);
 
         UserController userController = new UserController();
 
         //User Login
-        app.post("/login", userController::login);
+        app.post("/login", userController::login); //Default path
 
         //Employee Methods
-        app.get("/reimbs", userController::pastReimbs);
+        app.get("/reimbs", userController::pastReimbs); //Default dashboard for Employees
 
         //Finance Manager Methods
+        //Login
+        app.post("/login/fm", userController::loginFM);
+
+        //Dashboard
         app.routes(() -> {
-            path("{username}/all", () -> {
+            path("{username}/all", () -> { //Default dashboard for Finance Managers
                 get(userController::allReimbs);
                 path("filter", () -> {
                     get(userController::reimbsByStatus);
